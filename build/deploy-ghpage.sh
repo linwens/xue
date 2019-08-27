5ca3e2f4ae9ade27e518bcda53ddb6bff169aff2
linwens_xdf <linwenshu@xdf.cn>
2019-08-27 16:05:28 +0800

    ci(travis): 改npm包名称

--- build/deploy-ghpage.sh
+++ build/deploy-ghpage.sh
@@ -11,5 +11,4 @@ git config user.name "$U_NAME"
 git config user.email "$U_EMAIL"
 git add .
 git commit -m "$COMMIT_M"
-echo "https://$GH_TOKEN@$GH_REF"
 git push --force --quiet "https://$GH_TOKEN@$GH_REF" master:$P_BRANCH
