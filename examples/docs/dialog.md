eb264760f902d96e1179b44298876cf6900b91c2
linwens <lws200841001@gmail.com>
2019-11-24 22:04:57 +0800

    卡在appendTobody，但是子组件里还是会加入

--- examples/docs/dialog.md
+++ examples/docs/dialog.md
@@ -7,12 +7,15 @@
 ### 使用方法
 <!-- {.md} -->
 
-这里是使用说明
+这里是使用说明，支持标题、内容、尾部内容的自定义；注意自定义页头会覆盖title字段的内容。
 
 <div class="demo-block">
   <xu-button type='text' @click="dialogVisible = true">打开对话框</xu-button>
   
   <xu-dialog title="提示" :visible.sync="dialogVisible" width="30%">
+    <span slot="header">
+      <div>自定义页头会覆盖title</div>
+    </span>
     <span>这是一段信息</span>
     <span slot="footer" class="dialog-footer">
       <xu-button @click="dialogVisible = false">取 消</xu-button>
@@ -30,11 +33,49 @@
 ```
 :::
 
+### 可拖动
+文字说明
+<div class="demo-block"></div>
+
+:::demo
+```html
+
+```
+
+:::
+
+### 页面间停留
+将对话框插入的body元素下，当希望切换页面并且保留弹窗时，非常有用。注意，当前情况下不显示遮罩。
+
+<div class="demo-block">
+  <xu-button type='text' @click="dialogVisible = true">打开对话框</xu-button>
+  
+  <xu-dialog
+    title="插入到body"
+    :visible.sync="dialogVisible"
+    width="30%"
+    appendToBody>
+    <span>这是一段信息</span>
+    <span slot="footer" class="dialog-footer">
+      <xu-button @click="dialogVisible = false">取 消</xu-button>
+      <xu-button type="primary" @click="dialogVisible = false">确 定</xu-button>
+    </span>
+  </xu-dialog>
+</div>
+
+:::demo
+```html
+
+```
+
+:::
+
 ## Attributes
 <!-- {.md} -->
 | 参数      | 说明    | 类型      | 可选值       | 默认值   |
 |---------- |-------- |---------- |-------------  |-------- |
-| name     | 图标名称  | string  | -          |    -     |
+| title     | 标题名称  | string  | -          |    -     |
+| show-close | 显示关闭按钮 | boolean | true/false | true |
 
 <script>
   export default{
--- packages/dialog/src/dialog.vue
+++ packages/dialog/src/dialog.vue
@@ -1,10 +1,18 @@
 <template>
   <div>
-    <div v-if="visible" class="xu-dialog" :style="{width: width}">
+    <div 
+      v-if="visible"
+      class="xu-dialog"
+      :style="{width: width}">
       <div class="xu-dialog__header">
         <slot name="header">
           <span>{{title}}</span>
         </slot>
+        <xu-button 
+          type="text"
+          @click="handleClose"
+          class="xu-dialog_headerbtn"
+          v-if="showClose">x</xu-button>
       </div>
       <div class="xu-dialog__body">
         <slot></slot>
@@ -13,7 +21,7 @@
         <slot name="footer"></slot>
       </div>
     </div>
-    <div v-if="visible && mask" class="u-mask"></div>
+    <div v-if="visible && mask && !appendToBody" class="u-mask"></div>
   </div>
 </template>
 
@@ -41,11 +49,58 @@
       mask: {
         type: Boolean,
         default: true
-      }
+      },
+      appendToBody: {
+        type: Boolean,
+        default: false
+      },
+      closeOnClickModal: {
+        type: Boolean,
+        default: true
+      },
+      showClose: {
+        type: Boolean,
+        default: true
+      },
+      top: {
+        type: String,
+        default: '15vh'
+      },
+      beforeClose: Function,
     },
     data(){
       return {
-        
+        closed: false
+      }
+    },
+    watch: {
+      visible(val) {
+        if (val) {
+          if (this.appendToBody) { // 添加到body下面
+            document.body.appendChild(this.$el)
+          }
+        }
+      }
+    },
+    methods: {
+      handleClose() {
+        if (typeof this.beforeClose === 'function') {
+          this.beforeClose(this.hide);
+        } else {
+          this.hide();
+        }
+      },
+      hide(cancel) {
+        if (cancel !== false) {
+          this.$emit('update:visible', false);
+          this.$emit('close');
+          this.closed = true;
+        }
+      },
+    },
+    destroyed(){
+      if (this.appendToBody && this.$el && this.$el.parentNode) {
+        this.$el.parentNode.removeChild(this.$el);
       }
     }
   };
